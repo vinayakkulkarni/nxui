6bc4f11899b600c24542bc230de725476721a9c3
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-18 11:36:14 +0530

    refactor: use shadcn-vue Table in ProseTable instead of raw table element
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- app/components/content/ProseTable.vue
+++ app/components/content/ProseTable.vue
@@ -1,18 +1,9 @@
-<template>
-  <div class="my-6 w-full overflow-x-auto rounded-lg border">
-    <table>
-      <slot></slot>
-    </table>
-  </div>
-</template>
+<script setup lang="ts">
+  import { Table } from '~/components/ui/table';
+</script>
 
-<style scoped>
-  table {
-    width: 100%;
-    text-align: left;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    border-collapse: separate;
-    border-spacing: 0;
-  }
-</style>
+<template>
+  <Table class="my-6 rounded-lg border text-left">
+    <slot></slot>
+  </Table>
+</template>
