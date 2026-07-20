b9e323cf451ec3d0902b2970496239a157d39357
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-07-20 16:05:11 +0000

    docs: add signature eraser demo and guide
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ app/components/content/DemoSignatureEraser.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import SignatureEraser from '@registry/new-york/signature-eraser/SignatureEraser.vue';
+</script>
+
+<template>
+  <ComponentDemo
+    :code="`<script setup lang=&quot;ts&quot;>
+  import SignatureEraser from '~/components/ui/signature-eraser/SignatureEraser.vue';
+</script>
+
+<template>
+  <SignatureEraser class=&quot;h-100&quot; />
+  <!-- Draw with the pen, wipe with the eraser, or hit Clear
+       to disintegrate the signature into particles -->
+</template>`"
+  >
+    <div class="relative size-full min-h-80">
+      <SignatureEraser class="absolute inset-0 size-full" />
+    </div>
+  </ComponentDemo>
+</template>
